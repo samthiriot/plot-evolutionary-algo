fa7d74d54dbcdb536ae8590a0aca83186cf64f09
Sam Thiriot <samuel.thiriot@res-ear.ch>
2020-09-02 16:43:38 +0200

    added packing script

---
+++ package.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+VERSION=`date +"%Y-%m-%d-%H-%M"`
+
+FILENAME=shiny-app-$VERSION
+
+WD=`pwd`
+
+mkdir -p /tmp/build-shiny-app
+
+cp LICENSE plot-evolution/app.R /tmp/build-shiny-app/
+
+cd /tmp/build-shiny-app
+
+zip -r9 $WD/$FILENAME LICENSE app.R 
+
+cd $WD 
+
+rm -rf /tmp/build-shiny-app
+
+ls -lah $FILENAME.zip
+
